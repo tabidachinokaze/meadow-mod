fa5d84ba8a174e55152688785c2dd9cbd1d08711
tabidachinokaze <kaze@tabidachi.moe>
2026-08-09 07:41:02 +0800

    initial commit

---
+++ src/client/kotlin/moe/tabidachi/client/MeadowClient.kt
@@ -0,0 +1,9 @@
+package moe.tabidachi.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object MeadowClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/moe/tabidachi/client/MeadowDataGenerator.kt
@@ -0,0 +1,9 @@
+package moe.tabidachi.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MeadowDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
